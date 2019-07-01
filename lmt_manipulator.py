5eb6a9e7c5316c1d6a0bcb66b49943416a56b701
Strackeror <rremilavergne@gmail.com>
2019-07-01 22:23:25 +0200

    manipulator tweaks

--- lmt_manipulator.py
+++ lmt_manipulator.py
@@ -1,20 +1,28 @@
 import sys
+import struct
 import json
 from lmt import Lmt
+
+def aob_hexstr(type):
+    return struct.pack('%dB' % len(type), *type).hex()
+    
+def hexstr_aob(hex):
+    aob = bytes.fromhex(hex)
+    return list(struct.unpack('%dB' % len(aob), aob))
 
 def events_to_json(events):
     events_dict = { "unkn": events.unkn } 
     categories = []
     for category in events.categories:
-        category_dict = {"type":category.type}
+        category_dict = {"type":aob_hexstr(category.type)}
         events = []
         for event in category.events:
             event_dict = {
-                "type":event.type,
+                "type":aob_hexstr(event.type),
                 "buffer":[]
             }
             for buffer in event.buffer:
-                event_dict["buffer"] += [buffer.values]
+                event_dict["buffer"] += [aob_hexstr(buffer.values)]
             events += [event_dict]
         category_dict["events"] = events
         categories += [category_dict]
@@ -26,13 +34,13 @@ def json_to_events(json_str):
     events = Lmt.Events(unkn = events_dict["unkn"], categories_offset = 0, category_count = 0)
     events.categories = []
     for category_dict in events_dict["categories"]:
-        category = Lmt.Events.Event(type=category_dict["type"], offset = 0, count = 0)
+        category = Lmt.Events.Event(type=hexstr_aob(category_dict["type"]), offset = 0, count = 0)
         category.events =  []
         for event_dict in category_dict["events"]:
-            event = Lmt.Events.Event(type=event_dict["type"], offset = 0, count = 0)
+            event = Lmt.Events.Event(type=hexstr_aob(event_dict["type"]), offset = 0, count = 0)
             event.buffer = []
             for buffer in event_dict["buffer"]:
-                event.buffer += [Lmt.Events.Data(values = buffer)]
+                event.buffer += [Lmt.Events.Data(values = hexstr_aob(buffer))]
             category.events += [event]
         events.categories += [category]
     return events
@@ -89,8 +97,15 @@ def import_events(params):
     animation.events = json_to_events(open(path, 'r').read())
     open(output_path, 'wb').write(animation.serialize(0))
 
+usage = '''
+lmt_manipulator.py export_animation %pathToLmt %animationId %animationFile : extract one animation from a lmt file
+lmt_manipulator.py override_animation %animationFile %pathToLmt %animationId : override one animation with a given animation file
+lmt_manipulator.py export_events %animationFile %jsonEvents: export the metadata (I call them events now) from an animation file to json
+lmt_manipulator.py import_events %jsonEvents %animationFile: import json events to an animation file
+'''
 if __name__ == "__main__":
     if (len(sys.argv) < 2):
+        print(usage)
         exit()
     {
         "export_animation":export_animation,
